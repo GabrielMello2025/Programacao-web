fd780947db07b53bfa894baad09c22c52f919b3e
GABRIEL DA SILVA DE MELO BRANDAO <gabriel.smbrandao@senacsp.edu.br>
2025-09-24 21:18:46 -0300

    Primeiro commit

---
+++ Aula_24_09_2025/script.js
@@ -0,0 +1,19 @@
+    document.getElementById('btnEnviar').innerText = 'Enviar para nasa';
+
+for(let i = 0; i< 1000; i++)
+{
+    console.log(i);
+   
+    // alert('teste 1234')
+
+    // if (i == 0) {
+    //     alert ('Faça o pix');
+    // }
+    // else ( i == 1)
+    // {
+    //     alert ('Estou te rackeando');
+    // }
+
+}
+
+
